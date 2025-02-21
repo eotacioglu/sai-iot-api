0cda7a421cce8678c0d6f7f1e99bfd754f8989ee
Emre <eotacioglu@gmail.com>
2025-02-22 02:30:09 +0300

    UPDATE

--- index.js
+++ index.js
@@ -61,6 +61,49 @@ app.get("/devices", async (req, res) => {
         res.status(500).json({ message: "Sunucu hatası" });
     }
 });
+app.post("/devices/update", async (req, res) => {
+    try {
+
+        const { _id, deviceName } = req.body; // JSON'dan id, name ve cardId al
+
+        if (!_id) {
+            return res.status(400).json({ error: "Güncelleme için ID gereklidir" });
+        }
+
+        const updatedDevice = await Device.findByIdAndUpdate(
+            _id,
+            { deviceName },
+            { new: true } // Güncellenmiş veriyi döndür
+        );
+
+        if (!updatedDevice) {
+            return res.status(404).json({ error: "Aygıt bulunamadı" });
+        }
+
+        res.json({ message: "Kullanıcı güncellendi", device: updatedDevice });
+    } catch (error) {
+        res.status(500).json({ error: "Sunucu hatası" });
+    }
+});
+app.post("/devices/delete", async (req, res) => {
+    try {
+        const { _id } = req.body; // JSON'dan id al
+
+        if (!_id) {
+            return res.status(400).json({ error: "Silme işlemi için ID gereklidir" });
+        }
+
+        const deletedDevice = await Device.findByIdAndDelete(_id);
+
+        if (!deletedDevice) {
+            return res.status(404).json({ error: "Aygıt bulunamadı" });
+        }
+
+        res.json({ message: "Aygıt silindi" });
+    } catch (error) {
+        res.status(500).json({ error: "Sunucu hatası" });
+    }
+});
 
 app.get("/users", async (req, res) => {
     try {
@@ -91,6 +134,62 @@ app.get('/processes', async (req, res) => {
     } catch (error) {
         console.error("❌ Hata:", error);
         res.status(500).json({ error: "Sunucu hatası!" });
+    }
+});
+app.post("/users", async (req, res) => {
+    try {
+        console.log(req.body)
+        const { name, cardId } = req.body;
+        const user = new User({ name, cardId });
+        await user.save();
+        res.status(200).json({ message: "Kullanıcı eklendi", user });
+    } catch (error) {
+        res.status(500).json({ error: "Sunucu hatası" });
+    }
+});
+
+app.post("/users/update", async (req, res) => {
+    try {
+
+        const { _id, name, cardId } = req.body; // JSON'dan id, name ve cardId al
+
+        if (!_id) {
+            return res.status(400).json({ error: "Güncelleme için ID gereklidir" });
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            _id,
+            { name, cardId },
+            { new: true } // Güncellenmiş veriyi döndür
+        );
+
+        if (!updatedUser) {
+            return res.status(404).json({ error: "Kullanıcı bulunamadı" });
+        }
+
+        res.json({ message: "Kullanıcı güncellendi", user: updatedUser });
+    } catch (error) {
+        res.status(500).json({ error: "Sunucu hatası" });
+    }
+});
+
+app.post("/users/delete", async (req, res) => {
+    try {
+        const { _id } = req.body; // JSON'dan id al
+
+        if (!_id) {
+            return res.status(400).json({ error: "Silme işlemi için ID gereklidir" });
+        }
+
+        const deletedUser = await User.findByIdAndDelete(_id);
+
+        if (!deletedUser) {
+            return res.status(404).json({ error: "Kullanıcı bulunamadı" });
+        }
+
+        res.json({ message: "Kullanıcı silindi" });
+    } catch (error) {
+        res.status(500).json({ error: "Sunucu hatası" });
     }
 });
 
