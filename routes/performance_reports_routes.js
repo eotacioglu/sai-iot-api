21a3c7446c9a8f40956d765d6b2b81ec56edea03
kaanemre1406 <kaanemre1406@gmail.com>
2025-03-09 20:00:51 +0300

    up

--- routes/performance_reports_routes.js
+++ routes/performance_reports_routes.js
@@ -1,6 +1,7 @@
 // routes/performance_reports_routes.js.js
 const express = require("express");
 const { Process } = require("../schemas/Process"); // Operations modelini içe aktar
+const { Device } = require("../schemas/Device"); // Operations modelini içe aktar
 
 const router = express.Router();
 // gün süresi	:	540 dk
@@ -35,23 +36,49 @@ router.get("/", async (req, res) => {
 
     try {
         const process = await Process.find();
+
         for (let i = 0; i < process.length; i++) {
             let performance = {};
             // Tarihleri JavaScript Date nesnesine çevir
-            let dateObjects = process[i].processDates.map(date => new Date(date));
+            const dateObjects = process[i].processDates.map(date => new Date(date)).sort((a, b) => a - b);
 
             // En erken ve en geç tarihi bul
-            let minDate = new Date(Math.min(...dateObjects));
-            let maxDate = new Date(Math.max(...dateObjects));
-            let resultData;
+            const minDate = dateObjects[0];
+            const maxDate = dateObjects[dateObjects.length - 1];
 
-            // Farkı dakika cinsinden hesapla
-            let totalMinutes = Math.floor((maxDate - minDate) / (1000 * 60));
+            // Toplam süreyi dakika cinsinden hesapla
+            const totalMinutes = Math.floor((maxDate - minDate) / (1000 * 60));
+
+            // Kaybolan zamanı hesapla
+            let lostMinutes = 0;
+            for (let i = 1; i < dateObjects.length; i++) {
+                lostMinutes += Math.floor((dateObjects[i] - dateObjects[i - 1]) / (1000 * 60));
+            }
+
+            // Çalışılan zamanı hesapla
+            const activeMinutes = totalMinutes - lostMinutes;
 
             console.log(`Toplam çalışma süresi: ${totalMinutes} dakika`);
-            performance.totalMinutes = totalMinutes;
-            resultData = ((process[i].operation.defaultTime * process[i].processCount) / totalMinutes) * 100;
-            performanceList.push(resultData);
+            console.log(`Toplam kayıp zaman: ${lostMinutes} dakika`);
+            console.log(`Gerçek aktif çalışma süresi: ${activeMinutes} dakika`);
+            console.log(`Toplam çalışma süresi: ${totalMinutes} dakika`);
+            const device = await Device.findOne({ deviceId: process[i].device.deviceId }).populate("conveyor");
+
+            /// totalMinuts verildiğinde kulanıcının gün içerisindeki mesai süresi
+            ///540 verildiğinde işletmenin standart mesai süresi
+            performance.processResult = ((process[i].operation.defaultTime * process[i].processCount) / 540) * 100;
+            performance.reelProcessResult = ((process[i].operation.defaultTime * process[i].processCount) / activeMinutes) * 100;
+            performance.operationName = process[i].operation.name;
+            performance.userName = process[i].user.name;
+            performance.defaultTime = process[i].operation.defaultTime;
+            performance.processCount = process[i].processCount;
+            performance.conveyorName = device.conveyor.name;
+            performance.processTime = totalMinutes;
+            performance.lostMinutes = lostMinutes;
+            performance.activeMinutes = activeMinutes;
+
+
+            performanceList.push(performance);
         }
         res.json(performanceList);
     } catch (err) {
